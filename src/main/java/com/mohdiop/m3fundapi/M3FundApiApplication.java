25d1ceaad12a589df66fbca192cdc843c65450e2
Mohamed Diop <mohdiopcode@gmail.com>
2025-10-09 14:39:58 +0000

    Initialise le projet

---
+++ src/main/java/com/mohdiop/m3fundapi/M3FundApiApplication.java
@@ -0,0 +1,13 @@
+package com.mohdiop.m3fundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3FundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3FundApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohdiop/m3fundapi/M3FundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohdiop.m3fundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3FundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
